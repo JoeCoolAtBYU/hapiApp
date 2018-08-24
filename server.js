da5f78dcbf508000b7b8a9363e208bb3a615e9a0
Joshua Barnett <joecoolatbyu@ldschurch.org>
2018-08-24 12:23:54 -0600

    A simple Hello route

--- server.js
+++ server.js
@@ -2,5 +2,18 @@ var Hapi = require('hapi');
 
 var server = new Hapi.Server();
 
-server.connection({port:3000});
+server.connection({port: 3000});
+
+server.route({
+    path: '/hello',
+    method: 'GET',
+    handler: function (request, reply) {
+        reply('Hello World');
+    }
+});
+
+server.start(function () {
+    console.log('Listenting on ' + server.info.uri);
+});
+
 
